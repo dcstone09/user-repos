b1d77faa88f1e31fd605f4a72e651b3140e68d5e
David Stone <dcstone09@gmail.com>
2022-06-20 07:28:38 -0700

    Updates

---
+++ _next/static/zw6javRB8yPR9LxV2NK1o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/329-df89f0629a70102b.js","static/chunks/pages/index-0608b8ae7ba5485f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
